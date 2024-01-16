2a8e2d6cacbf01688dc1cca82d78e15b7f0a68a3
Esmee te Winkel <74557272+esmeetewinkel@users.noreply.github.com>
2024-01-16 14:17:20 +0000

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,12 +1,41 @@
-import { extendDeploymentConfig } from "scripts";
-const config = extendDeploymentConfig({ name: "plh_facilitator_tz", parent: "plh_facilitator" });
+import { generateDeploymentConfig } from "scripts";
+const config = generateDeploymentConfig("plh_facilitator_tz");
 
-// override app constants here
-config.app_config.APP_HEADER_DEFAULTS.title = "plh_facilitator_tz";
-config.app_config.APP_SIDEMENU_DEFAULTS.title = "plh_facilitator_tz";
+/**
+ * The default config should ideally be a superset of any extended configs
+ * to allow for easier post-processing
+ */
 
-// example filters applied to main deployment content
-config.app_data.sheets_filter_function = (flow) =>true,
-config.app_data.assets_filter_function = (fileEntry) =>true
+config.google_drive = {
+  sheets_folder_ids: ["1k8zJfBeOm18gYDsk0XmdUm7OFtZDUY9R", "1BbbC8afYsT6pDrBlqDu8q0TjgIQsnPlk"],
+  assets_folder_ids: ["1KcHDI7O4o2_FZ_YlXsz-8OqN3ehsfdVf", "1p12EIcM0zuA6s6Djvqxg1sdGint7TEWt"],
+};
+
+config.android = {
+  splash_asset_path: "./app_data/assets/android/splash.png",
+  icon_asset_path: "./app_data/assets/android/icon.png",
+  icon_asset_foreground_path: "./app_data/assets/android/icon-foreground.png",
+  icon_asset_background_path: "./app_data/assets/android/icon-background.png",
+};
+
+config.git = {
+  content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-my-content.git",
+  content_tag_latest: "1.1.1",
+};
+
+config.api.db_name = "plh_facilitator_tz"
+
+config.app_data.output_path = "./app_data";
+
+config.app_config.APP_LANGUAGES.default = "gb_en";
+config.app_config.APP_SIDEMENU_DEFAULTS.title = "FacilitatorApp TZ";
+config.app_config.APP_HEADER_DEFAULTS.title = "FacilitatorApp TZ";
+config.app_config.NOTIFICATION_DEFAULTS.title = "New message from FacilitatorApp TZ";
+config.app_config.NOTIFICATION_DEFAULTS.text = "You have a new message from FacilitatorApp TZ";
+config.app_config.APP_THEMES.available = ["default", "professional"];
+config.app_config.APP_THEMES.defaultThemeName = "professional";
+config.app_config.APP_UPDATES.enabled = true;
+config.app_config.APP_UPDATES.completeUpdateTemplate = "app_update_complete";
+config.app_config.APP_FOOTER_DEFAULTS.templateName = "footer";
 
 export default config;
